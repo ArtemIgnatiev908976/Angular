cbbab84458901e95cae7cee639bbffb902b5731d
Artem <ignatev.artem.nik@gmail.com>
2021-05-11 23:15:24 +0400

    Пайпы
    Трансформируем элементы

--- Angular 9. Теория и практика/angular-basics/src/app/app.component.ts
+++ Angular 9. Теория и практика/angular-basics/src/app/app.component.ts
@@ -6,21 +6,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  arr = [1, 1, 2, 3, 5, 8, 13]
-
-  obj =[
-    {title: 'Post 1', author: 'Artem', comments: [
-        {name:'Max', text: 'lorem1'},
-        {name:'Max', text: 'lorem2'},
-        {name:'Max', text: 'lorem3'},
-      ]},
-
-    {title: 'Post 2', author: 'Artem2', comments: [
-        {name:'Max', text: 'lorem11'},
-        {name:'Max', text: 'lorem22'},
-        {name:'Max', text: 'lorem33'},
-      ]},
-  ]
-
-
+now: Date = new Date
 }
