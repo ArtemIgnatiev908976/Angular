ecbf78b9f24b3f49e8544882c7110c09727ab392
Artem <ignatev.artem.nik@gmail.com>
2021-05-11 23:08:37 +0400

    Циклы с ngFor

--- Angular 9. Теория и практика/angular-basics/src/app/app.component.ts
+++ Angular 9. Теория и практика/angular-basics/src/app/app.component.ts
@@ -6,5 +6,21 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-toggle: any = false
+  arr = [1, 1, 2, 3, 5, 8, 13]
+
+  obj =[
+    {title: 'Post 1', author: 'Artem', comments: [
+        {name:'Max', text: 'lorem1'},
+        {name:'Max', text: 'lorem2'},
+        {name:'Max', text: 'lorem3'},
+      ]},
+
+    {title: 'Post 2', author: 'Artem2', comments: [
+        {name:'Max', text: 'lorem11'},
+        {name:'Max', text: 'lorem22'},
+        {name:'Max', text: 'lorem33'},
+      ]},
+  ]
+
+
 }
