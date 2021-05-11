a25dabeb09e555e4bf24979a70d95229961d9897
Artem <ignatev.artem.nik@gmail.com>
2021-05-11 15:25:28 +0400

    2 Way Binding
    Двухсторонее связывание

--- Angular 9. Теория и практика/angular-basics/src/app/app.component.ts
+++ Angular 9. Теория и практика/angular-basics/src/app/app.component.ts
@@ -6,32 +6,8 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  title = 'Dynamic title'
-  number = 42
-  arr = [1,2,3]
-  obj = {a:1, b:{c:2}}
-  // img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png'
-  inputValue = ''
-
-
-  constructor() {
-    // setTimeout(()=>{
-    //   console.log('Time out is over')
-    //   this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png'
-    // },5000)
-  }
-
-  onInput(event:KeyboardEvent){
-    console.log('Event',event)
-    this.inputValue = (<HTMLInputElement>event.target).value
-
-  }
-onClick(){
-  console.log('Click')
+title = 'initial'
+onInput(event:any){
+  this.title = event.target.value
 }
-onBlur(str:string){
-this.inputValue = str
-}
-
 }
