8950bf573743e0f5c7daad311c315e6001aaf584
Artem <ignatev.artem.nik@gmail.com>
2021-05-11 15:14:16 +0400

    Как работает Event Bindings

--- Angular 9. Теория и практика/angular-basics/src/app/app.component.ts
+++ Angular 9. Теория и практика/angular-basics/src/app/app.component.ts
@@ -11,14 +11,27 @@ export class AppComponent {
   number = 42
   arr = [1,2,3]
   obj = {a:1, b:{c:2}}
-  img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png'
+  // img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png'
+  inputValue = ''
 
 
   constructor() {
-    setTimeout(()=>{
-      console.log('Time out is over')
-      this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png'
-    },5000)
+    // setTimeout(()=>{
+    //   console.log('Time out is over')
+    //   this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png'
+    // },5000)
   }
+
+  onInput(event:KeyboardEvent){
+    console.log('Event',event)
+    this.inputValue = (<HTMLInputElement>event.target).value
+
+  }
+onClick(){
+  console.log('Click')
+}
+onBlur(str:string){
+this.inputValue = str
+}
 
 }
