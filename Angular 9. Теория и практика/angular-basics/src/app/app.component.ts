7e272bc658b45abef881173ef73af54d37bc51fe
Artem <ignatev.artem.nik@gmail.com>
2021-05-11 14:50:06 +0400

    Bindings
    Смена картинки через 5 секунд

--- Angular 9. Теория и практика/angular-basics/src/app/app.component.ts
+++ Angular 9. Теория и практика/angular-basics/src/app/app.component.ts
@@ -6,8 +6,19 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'Dynamic title'
   number = 42
   arr = [1,2,3]
   obj = {a:1, b:{c:2}}
+  img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png'
+
+
+  constructor() {
+    setTimeout(()=>{
+      console.log('Time out is over')
+      this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png'
+    },5000)
+  }
+
 }
