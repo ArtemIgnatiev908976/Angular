85e3f5f57538570445ba3447ebe3638b4ffd03db
Artem <ignatev.artem.nik@gmail.com>
2021-05-20 00:07:40 +0400

    создание объектов с POST

--- Angular 9. Теория и практика/angular-basics/src/app/app.component.ts
+++ Angular 9. Теория и практика/angular-basics/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import {Component, OnInit} from '@angular/core'
-import {HttpClient, HttpClientModule} from "@angular/common/http";
-export interface Todo{
+import {HttpClient} from '@angular/common/http'
+
+export interface Todo {
   completed: boolean
   title: string
   id?: number
-
 }
 
 @Component({
@@ -13,16 +13,37 @@ export interface Todo{
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
   todos: Todo[] = []
+
+  todoTitle = ''
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
-      .subscribe(todos=>{
-        console.log(todos)
+      .subscribe(todos => {
+        console.log('Response', todos)
         this.todos = todos
       })
+  }
+
+  addTodo() {
+    if (!this.todoTitle.trim()) {
+      return
+    }
+
+    const newTodo: Todo = {
+      title: this.todoTitle,
+      completed: false
+    }
+
+    this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo)
+      .subscribe(todo => {
+        this.todos.push(todo)
+        this.todoTitle = ''
+      })
+
   }
 }
 
