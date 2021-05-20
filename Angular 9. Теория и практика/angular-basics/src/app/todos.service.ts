d7d2a993e8a0cb9142e806eae40a70dc8d90dbe1
Artem <ignatev.artem.nik@gmail.com>
2021-05-20 18:54:57 +0400

    Работа с параметрами

--- Angular 9. Теория и практика/angular-basics/src/app/todos.service.ts
+++ Angular 9. Теория и практика/angular-basics/src/app/todos.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core'
-import {HttpClient, HttpHeaders} from '@angular/common/http'
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http'
 import {Observable, throwError} from 'rxjs'
 import {catchError, delay} from 'rxjs/operators'
 import {error} from "@angular/compiler/src/util";
@@ -25,10 +25,17 @@ export class TodosService {
   }
 
   fetchTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+    let params = new HttpParams()
+    params = params.append('_limit', '4')
+    params = params.append('custom','anything' )
+
+    return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos', {
+      // params:new HttpParams().set('_limit','3')
+      params: params
+    })
       .pipe(
         delay(500),
-        catchError(error=>{
+        catchError(error => {
           console.log('Error', error.message)
           return throwError(error)
         })
