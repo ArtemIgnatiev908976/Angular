111aa55e52c98d576f3f2a251b18864c093b74dc
Artem <ignatev.artem.nik@gmail.com>
2021-05-25 14:45:45 +0400

    Фикс для ленивой загрузки, работает на ангуляр 9+

--- Angular 9. Теория и практика/angular-blog/src/app/app-routing.module.ts
+++ Angular 9. Теория и практика/angular-blog/src/app/app-routing.module.ts
@@ -13,9 +13,7 @@ const routes: Routes = [
       {path: 'post/:id', component: PostPageComponent}
     ]
   },
-  {
-    path: 'admin', loadChildren: './admin/admin.module#AdminModule'
-  }
+  {     path: "admin",     loadChildren: () => import("./admin/admin.module").then(m => m.AdminModule)   }
 ];
 
 @NgModule({
