36824713b789b5a549d0aae3a30e33e5f1c0b0d6
Artem <ignatev.artem.nik@gmail.com>
2021-05-27 14:14:01 +0400

    Обработка ошибок

--- Angular 9. Теория и практика/angular-blog/src/app/admin/login-page/login-page.component.ts
+++ Angular 9. Теория и практика/angular-blog/src/app/admin/login-page/login-page.component.ts
@@ -15,7 +15,7 @@ export class LoginPageComponent implements OnInit {
   submitted = false
 
   constructor(
-    private auth: AuthService,
+    public auth: AuthService,
     private router: Router
   ) {
   }
@@ -49,7 +49,7 @@ export class LoginPageComponent implements OnInit {
       this.form.reset()
       this.router.navigate(['/admin', 'dashboard'])
       this.submitted = false
-    })
+    }, ()=> {this.submitted=false} )
   }
 }
 
